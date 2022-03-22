b564f98e159b11c86f6b51fcd10c24ef05bdabdc
MrRad1k <68542732+MrRad1k@users.noreply.github.com>
2022-03-22 19:33:12 +0300

    project

---
+++ routers/userRouter.js
@@ -0,0 +1,10 @@
+const Router = require('express')
+const router = new Router()
+const userController = require('../controller/userController')
+
+//router.post('/reg', userController.reg)
+//router.post('/login', userController.login)
+router.get('/auth', userController.auth)
+router.get('/:id', userController.getOne)
+
+module.exports = router
