6a194179109c0b353eeba38ef0613a555d9d32a3
MrRad1k <68542732+MrRad1k@users.noreply.github.com>
2022-03-22 13:35:43 +0300

    p

--- routers/index.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const reviewRouter = require('./reviewRouter')
-const userRouter = require('./userRouter')
-
-router.use('/user', userRouter)
-router.use('/review', reviewRouter)
-
-module.exports = router
