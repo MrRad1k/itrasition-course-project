e051dfeceadfbd90fd20fb027a47180407030747
MrRad1k <68542732+MrRad1k@users.noreply.github.com>
2022-03-22 21:52:53 +0300

    project

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,13 +6,13 @@ import { Link } from "react-router-dom";
             /> */
 const Navbar = ({ user }) => {
   const logout = () => {
-    window.open("http://localhost:5000/auth/logout", "_self");
+    window.open("https://itrasition-course-project.herokuapp.com/auth/logout", "_self");
   };
   return (
     <div className="navbar">
       <span className="logo">
         <Link className="link" to="/">
-          Lama App
+           App
         </Link>
       </span>
       {user ? (
--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://itrasition-course-project.herokuapp.com",
     methods: "GET, POST, PUT, DELETE",
     credentials: true
 }))
