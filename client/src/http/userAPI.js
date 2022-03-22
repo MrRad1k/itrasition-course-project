e734fdadcf98a055eaf5c3f34b0b5fd8156a4862
MrRad1k <68542732+MrRad1k@users.noreply.github.com>
2022-03-22 19:18:03 +0300

    project

--- client/src/http/userAPI.js
+++
@@ -1,7 +0,0 @@
-import {$authHost, $host} from "./index";
-
-export const check = async () => {
-    const {data} = await $authHost.get('api/user/auth' )
-    
-    return data
-}
--- routers/reviewRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const reviewControlller = require('../controller/reviewController')
-
-router.post('/', reviewControlller.create)
-router.get('/', reviewControlller.getAll)
-router.get('/:id', reviewControlller.getOne)
-
-module.exports = router
